e6293aca9308fc0eaa78943d159aee62bc7b5103
Alfred Melson <avm@hmtgp.com>
2022-02-03 07:14:23 +0200

    struturual changes

--- src/components/tab-panels/DataResponse.tsx
+++ src/components/tab-panels/DataResponse.tsx
@@ -1,10 +1,40 @@
-import { Suspense, useEffect, useState } from 'react'
+import { AnimatePresence, motion, usePresence } from 'framer-motion'
+import { ReactNode, Suspense, useEffect, useState } from 'react'
 import { useRecoilValue } from 'recoil'
 import { ErrorBoundary } from '../../lib'
 import { AxiosResponseAlias, currentApiQuerySelector } from '../../recoil-state'
 import { PaperSx } from '../mui'
 import { getType } from './data-types'
 import { SortByType } from './primitive-styles'
+
+const transition = { type: 'spring', stiffness: 500, damping: 50, mass: 1 }
+
+type ListItems = {
+  children: ReactNode
+}
+
+function ListItem({ children }: ListItems) {
+  const [isPresent, safeToRemove] = usePresence()
+
+  const animations = {
+    layout: true,
+    initial: 'out',
+    sx: {
+      position: isPresent ? 'static' : 'absolute'
+    },
+    animate: isPresent ? 'in' : 'out',
+    whileTap: 'tapped',
+    variants: {
+      in: { scaleY: 1, opacity: 1 },
+      out: { scaleY: 0, opacity: 0, zIndex: -1 },
+      tapped: { scale: 0.98, opacity: 0.5, transition: { duration: 0.1 } }
+    },
+    onAnimationComplete: () => !isPresent && safeToRemove(),
+    transition
+  }
+
+  return <motion.div {...animations}>{children}</motion.div>
+}
 
 export default function DataResponse() {
   const currentApiQuery = useRecoilValue(currentApiQuerySelector)
@@ -27,13 +57,15 @@ export default function DataResponse() {
   const renderData = () => {
     return keys.map((key: string, index: number) => {
       return (
-        <SortByType
-          index={index}
-          key={index}
-          dataType={currentData && getType(currentData[key])}
-          dataValue={currentData && currentData[key]}
-          dataKey={key}
-        />
+        <ListItem key={index}>
+          <SortByType
+            index={index}
+            key={index}
+            dataType={currentData && getType(currentData[key])}
+            dataValue={currentData && currentData[key]}
+            dataKey={key}
+          />
+        </ListItem>
       )
     })
   }
@@ -41,7 +73,9 @@ export default function DataResponse() {
   return (
     <PaperSx>
       <Suspense fallback={<div>Loading...</div>}>
-        <ErrorBoundary>{renderData()}</ErrorBoundary>
+        <ErrorBoundary>
+          <AnimatePresence>{renderData()}</AnimatePresence>
+        </ErrorBoundary>
       </Suspense>
     </PaperSx>
   )
